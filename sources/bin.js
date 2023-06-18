84ab364c368abd972aec1d555220fe3f412510a0
Alexandr <digital-mag@ya.ru>
2023-06-18 18:57:48 +0300

    prevent git hooks error (if uninstalled)

--- sources/bin.js
+++ sources/bin.js
@@ -19,11 +19,29 @@ if (process.argv.indexOf('--config')) {
     }    
     fs.writeFileSync(PACKAGE_PATH, JSON.stringify(packageInfo, null, 2))
 
+    
+    
+    
     const gitConfigured = execSync('git config core.hooksPath .git/hooks/').toString()
     console.log(gitConfigured);
 
-    const hooksConfigured = execSync('npx simple-git-hooks').toString()
-    console.log(hooksConfigured);
+
+
+    try {
+        const hooksConfigured = execSync('npx simple-git-hooks').toString();
+        console.log(hooksConfigured);
+    }
+    catch (er) {
+        if (fs.existsSync(path.join(process.cwd(), 'pnpm-lock.yaml'))) {
+            console.log(execSync('pnpm i -D simple-git-hooks').toString())
+        }
+        else {
+            console.log(execSync('npm i -D simple-git-hooks').toString())
+        }
+
+        const hooksConfigured = execSync('npx simple-git-hooks').toString();
+        console.log(hooksConfigured);
+    }
 }
 else {
     const filters = process.argv.filter(w => w.startsWith('--filter='))
